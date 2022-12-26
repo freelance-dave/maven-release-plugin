2dfd16491c0241682110686a596e99bcac5aa715
David Tozer <cpp.sage@gmail.com>
2022-12-26 11:48:33 +0000

    Section 11: Assignment Review

---
+++ Section_11/mb2g-spring-boot-assignment/web-app/src/main/java/guru/springframework/json/springbootmmjson/Mb2gSpringBootAssignmentApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.json.springbootmmjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mb2gSpringBootAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mb2gSpringBootAssignmentApplication.class, args);
+    }
+
+}
---
+++ Section_11/mb2g-spring-boot-assignment/web-app/src/test/java/guru/springframework/json/springbootmmjson/Mb2gSpringBootAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.json.springbootmmjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mb2gSpringBootAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
